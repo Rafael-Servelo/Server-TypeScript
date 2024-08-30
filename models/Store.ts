2373df38f24f48b0d88c689ed4f6e9b529a20fe7
Rafael <rafael.servelo@dminer.com.br>
2024-08-30 19:22:13 -0300

    iniciado a estrutura de postar produtos

--- models/Store.ts
+++ models/Store.ts
@@ -4,7 +4,20 @@ const Store = mongoose.model("Store", {
   id: Number,
   product: String,
   datePost: Date,
-  images: Array<string[]>
+  images: Array<string[]>,
+  tags: Array<string[]>,
+  price: String,
+  description: String,
+  numberSold: Number,
+  numberReview: Number,
+  promotion: Boolean,
+  rating: Number,
+  amount: Number,
+  category: String,
+  specifications: String,
+  sizes: Array<string[]>,
+  colors: Array<string[]>
+
 } as any);
 
 export default Store;
