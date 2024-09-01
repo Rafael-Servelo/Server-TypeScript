ac95a32dd6cf1f84e2f17acdc1774f4d7875ba2d
Rafael dos Santos Servelo <rafael.servelo@hotmail.com>
2024-09-01 01:44:34 -0300

    update model store

--- models/Store.ts
+++ models/Store.ts
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const Store = mongoose.model("Store", {
   id: Number,
   product: String,
+  width: Number,  // Largura (cm)
+  height: Number, // Altura (cm)
+  length: Number, // Comprimento (cm)
+  weight: Number, // Peso (kg)
   datePost: Date,
   images: Array<string[]>,
   tags: Array<string[]>,
@@ -16,8 +20,7 @@ const Store = mongoose.model("Store", {
   category: String,
   specifications: String,
   sizes: Array<string[]>,
-  colors: Array<string[]>
-
+  colors: Array<string[]>,
 } as any);
 
 export default Store;
