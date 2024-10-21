9bdde806a1253e99c6255156ce2d3b9928a626dd
Rafael dos Santos Servelo <rafael.servelo@hotmail.com>
2024-10-21 03:05:20 -0300

    add socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 /**
  * @author Rafael Servelo
- * @description Projeto realizado para a loja Lua Minguante, onde eu mesmo fiz o front-end e o back-end.
+ * @description Projeto realizado para a loja Lua Minguante.
  */
-
-import dotenv from "dotenv";
 import "./db";
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import router from "../routers/authRouter";
+import storerouter from "../routers/storeRouter";
+import { Server } from "socket.io";
+import http from "http";
 
 dotenv.config();
 const app = express();
 const baseDir = `${__dirname}/public/`;
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  path: "/socket.io",
+  cors: {
+    origin: "*"
+  }
+});
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -18,14 +28,19 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static(`${baseDir}`));
 app.get("/server", (req, res) => res.sendFile("index.html", { root: baseDir }));
 
-import router from "../routers/authRouter";
-import storerouter from "../routers/storeRouter";
-
 app.use("/server/auth", router);
 app.use("/server/store", storerouter);
 
 const port = process.env.PORT ?? 3000;
 
-app.listen(port, () => {
+io.on("connection", (socket)=>{
+  console.log(`Client connected ${socket.id}!`)
+
+  socket.on("disconnect", ()=>{
+    console.log(`Client ${socket.id} as disconnected!`)
+  })
+})
+
+httpServer.listen(port, () => {
   console.log(`O Servidor esta rodando na porta ${port}`);
 });
